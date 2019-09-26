7e52ccb5e1d6ed56f4886f7e55eb1a17b0f4f697
laura <laura.j.elias@gmail.com>
2019-09-26 13:40:37 -0500

    corrected the location of a require line in appjs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const { NODE_ENV } = require('./config')
 
 const app = express();
 
@@ -12,7 +13,6 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-const { NODE_ENV } = require('./config')
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!');
